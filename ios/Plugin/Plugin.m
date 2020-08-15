bc6a8c05856808b8972ae6f1626dac442c5a54ec
Roberto <roberto@villavanilla.com>
2020-08-15 15:11:26 -0500

    -exposing plugin methods to iOS

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,13 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(ConektaPlugin, "ConektaPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setPublicKey, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getPublicKey, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setLanguage, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getLanguage, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(createToken, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setApiVersion, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getApiVersion, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(deviceFingerPrint, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getBaseUri, CAPPluginReturnPromise);
 )

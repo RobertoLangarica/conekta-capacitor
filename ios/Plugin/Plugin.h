e26db79ee2440033c8b4cf2dddf6d0fa1ed06b8e
Roberto <roberto@villavanilla.com>
2020-08-15 10:56:32 -0500

    - iOS support

--- ios/Plugin/Plugin.h
+++ ios/Plugin/Plugin.h
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
+#import "Conekta.h"
 
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -7,11 +7,77 @@ import Capacitor
  */
 @objc(ConektaPlugin)
 public class ConektaPlugin: CAPPlugin {
-
+    var conekta = Conekta()
+     
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
+        
         call.success([
             "value": value
+        ])
+    }
+    
+    @objc func setPublicKey(_ call: CAPPluginCall){
+        let key = call.getString("key") ?? ""
+        conekta.publicKey = key
+        call.success()
+    }
+    
+    @objc func getPublicKey(_ call: CAPPluginCall){
+        call.success([
+            "key": conekta.publicKey ?? ""
+        ])
+    }
+    
+    @objc func setLanguage(_ call: CAPPluginCall){call.reject("Set langugage is not available for Conekta iOS")}
+    @objc func getLanguage(_ call: CAPPluginCall){call.reject("Get langugage is not available for Conekta iOS")}
+    
+    @objc func createToken(_ call: CAPPluginCall){
+        
+        let c_name = call.getString("name") ?? ""
+        let c_number = call.getString("number") ?? ""
+        let c_cvc = call.getString("cvc") ?? ""
+        let c_expMonth = call.getString("exp_month") ?? ""
+        let c_expYear = call.getString("exp_year") ?? ""
+        
+//        DispatchQueue.main.sync {
+//            self.conekta.collectDevice()
+//        }
+        
+//        conekta.delegate = self
+        conekta.collectDevice()
+        
+        let card = conekta.card()
+        
+        card?.setNumber(c_number, name: c_name, cvc: c_cvc , expMonth: c_expMonth, expYear: c_expYear)
+        
+        
+        let token = conekta.token()
+        token?.card = card
+        
+        
+        token?.create(success: { (data) -> Void in
+            if let data = data as NSDictionary? as! [String:Any]? {
+                call.success(data)
+            }
+        }, andError: { (error) -> Void in
+            //print(error)
+            call.reject(error.debugDescription, "", error)
+        })
+    }
+    
+    @objc func setApiVersion(_ call: CAPPluginCall){call.reject("Set Api version is not available for Conekta iOS")}
+    @objc func getApiVersion(_ call: CAPPluginCall){call.reject("Get Api version is not available for Conekta iOS")}
+    
+    @objc func deviceFingerPrint(_ call: CAPPluginCall){
+        call.success([
+            "fingerprint": conekta.deviceFingerprint() ?? ""
+        ])
+    }
+    
+    @objc func getBaseUri(_ call: CAPPluginCall){
+        call.success([
+            "fingerprint": conekta.baseURI ?? ""
         ])
     }
 }
